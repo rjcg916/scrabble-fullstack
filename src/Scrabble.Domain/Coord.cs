f99c39fedcd541736da1503f18485fcf8dcc89dd
Robert J. Graham <rjg916@hotmail.com>
2024-06-15 09:05:20 -0700

    Cleanup

--- src/Scrabble.Domain/Coord.cs
+++ src/Scrabble.Domain/Coord.cs
@@ -2,7 +2,6 @@
 
 namespace Scrabble.Domain
 {
-
     public enum R : int
     {
         _1 = 0, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15
@@ -32,38 +31,4 @@ namespace Scrabble.Domain
 
     public record LocationSquare (Coord Coord, Square Square);
 
-    //public class EvaluateAt<T>(int row, int col, T evaluator = default)
-    //{
-    //    public T Evaluator { get; set; } = evaluator;
-    //    public int Row { get; set; } = row;
-    //    public string RowName { get; set; } = ((R)row).ToString()[1..];
-    //    public int Col { get; set; } = col;
-    //    public string ColName { get; set; } = ((C)col).ToString()[0..];
-    //}
-
-  
-
-    public static class CoordExtensions
-    {
-
-        //public static Placement GetPlacementType(this List<Tile> tiles, Coord start, Coord end)
-        //{
-        //    bool horizontal = start.Row == end.Row;
-        //    int horizontalNumberOfTiles = end.Col - start.Col + 1;
-        //    bool vertical = start.Col == end.Col;
-        //    int verticalNumberOfTiles = end.Row - start.Row + 1;
-        //    bool oneTile = horizontal && vertical;
-        //    int numberOfTiles = tiles.Count;
-
-        //    if (horizontal && vertical && !oneTile)
-        //        throw new System.Exception("Unknown Placement Type");
-        //    if (horizontal && horizontalNumberOfTiles == numberOfTiles)
-        //        return Placement.Horizontal;
-        //    else if (vertical && verticalNumberOfTiles == numberOfTiles)
-        //        return Placement.Vertical;
-        //    else 
-        //        throw new System.Exception("Unsupported PlacementType");
-            
-        //}
-    }
 }
