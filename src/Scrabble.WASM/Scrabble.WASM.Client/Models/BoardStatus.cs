53398c2f61000e3a010709ee782c6cce893ffa5f
Robert J. Graham <rjg916@hotmail.com>
2024-07-06 18:40:23 -0700

    Cleanup

---
+++ src/Scrabble.WASM/Scrabble.WASM.Client/Models/BoardStatus.cs
@@ -0,0 +1,6 @@
+﻿using Scrabble.Domain;
+
+namespace Scrabble.WASM.Client.Models
+{
+  record BoardStatus(bool IsCurrentMoveValid, int CurrentMoveScore, string Messages, Move? CurrentMove = null);
+}
