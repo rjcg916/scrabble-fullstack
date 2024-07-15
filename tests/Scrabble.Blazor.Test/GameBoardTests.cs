0a77eca3ba6e7e5e0f070e13e8958cc604fcb17e
Robert J. Graham <rjg916@hotmail.com>
2024-07-15 05:04:41 -0700

    Extend GameBoardTests

--- tests/Scrabble.Blazor.Test/GameBoardTests.cs
+++ tests/Scrabble.Blazor.Test/GameBoardTests.cs
@@ -1,12 +1,13 @@
-using Microsoft.AspNetCore.Components;
-using MudBlazor;
-using MudBlazor.Charts;
+using AngleSharp.Dom;
+using FluentAssertions;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 using Scrabble.Domain;
 using Scrabble.WASM.Client.Components;
 using Scrabble.WASM.Client.Models;
 using System;
-using Telerik.JustMock;
-using MudBlazor.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Scrabble.Blazor.Test
 {
@@ -18,11 +19,11 @@ namespace Scrabble.Blazor.Test
         {
             Action<Move> onSomethingHandler = _ => { };
             
-            var ctx = new TestContext();
+            var ctx = new BunitContext();
             ctx.JSInterop.Mode = JSRuntimeMode.Loose;
             ctx.Services.AddMudServices();
 
-            var cut = ctx.RenderComponent<GameBoard>(parameters => parameters               
+            var cut = ctx.Render<GameBoard>(parameters => parameters               
               .Add(p => p.board, new ((s) => true) )
               .Add(p => p.rack, new Rack())
               .Add(p => p.CurrentPlayer, 1)
@@ -31,20 +32,51 @@ namespace Scrabble.Blazor.Test
 
             var init = cut.Instance;
             Assert.Equal(1, init.CurrentPlayer);
-
-            cut.SetParametersAndRender<GameBoard>(parameters => parameters
-                .Add(p => p.board, new((s) => true))
-                .Add(p => p.rack, new Rack())
+           
+            cut.Render<GameBoard>(parameters => parameters
                 .Add(p => p.CurrentPlayer, 2)
-                .Add(p => p.CurrentMoveChanged, onSomethingHandler)
             );
 
             var snd = cut.Instance;
-
+           
             Assert.Equal(2, snd.CurrentPlayer);
 
+        }
+
+        [Fact]
+        public void GameBoardDropZone_CorrectDropZones()
+        {
+            Action<Move> onSomethingHandler = _ => { };
+
+            var ctx = new BunitContext();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            ctx.Services.AddMudServices();
+   
+            var rack = new Rack();
+            var tilesToAdd = new List<Tile> { new('A'), new('B') };
+            rack = rack.AddTiles(tilesToAdd);
+
+            var cut = ctx.Render<GameBoard>(parameters => parameters
+              .Add(p => p.board, new((s) => true))
+              .Add(p => p.rack, rack)
+              .Add(p => p.CurrentPlayer, 1)
+              .Add(p => p.CurrentMoveChanged, onSomethingHandler)
+            );
+
+            var container = cut.Find(".mud-drop-container");
+            container.Children.Should().HaveCount(2);
+
+            cut.FindAll(".mud-drop-zone").Should().HaveCount(232);
+
+            var rackDropZones = container.Children[0];
+            rackDropZones.Children.Should().HaveCount(2);
+
+            var firstRackDropZoneA = rackDropZones.Children[0];
+            firstRackDropZoneA.TextContent.Should().Be("Tile Rack");
+
+            var boardRowDropZones = container.Children[1];
+            boardRowDropZones.Children.Should().HaveCount(16);
 
         }
-    
     }
 }
